7db508bc91cb21bd9b8c2684cc99e836aa310526
Henrique Paes de Souza <henriquee.paes1@gmail.com>
2023-01-23 17:03:13 -0300

    employeeDAO implementation, pending finds

--- src/model/dao/EmployeeDAO.java
+++ src/model/dao/EmployeeDAO.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DBException;
+import db.DBManager;
 import model.entities.Employee;
 
 public class EmployeeDAO implements IDAO<Employee> {
-	
+
 	private Connection conn;
 
 	public EmployeeDAO(Connection conn) {
@@ -15,16 +20,67 @@ public class EmployeeDAO implements IDAO<Employee> {
 
 	@Override
 	public void insert(Employee obj) {
-		
+		PreparedStatement query = null;
+		try {
+			query = conn
+					.prepareStatement("INSERT INTO franchises (name, startDate, email, department, wage, franchise_id) "
+							+ "VALUES (?, ?, ?, ?, ?, ?);");
+			query.setString(1, obj.getName());
+			query.setDate(2, obj.getStartDate());
+			query.setString(3, obj.getEmail());
+			query.setString(4, obj.getDepartment());
+			query.setDouble(5, obj.getWage());
+			query.setInt(6, obj.getFranchise().getId());
+
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DBManager.closeStatement(query);
+		}
+
 	}
 
 	@Override
 	public void update(Employee obj) {
-		
+		PreparedStatement query = null;
+
+		try {
+			query = conn.prepareStatement("UPDATE employees "
+					+ "SET name = ?, startDate = ?, email = ?, department = ?, wage = ?, franchise_id = ? " + 
+					"WHERE id = ?;",
+					Statement.RETURN_GENERATED_KEYS);
+
+			query.setString(1, obj.getName());
+			query.setDate(2, obj.getStartDate());
+			query.setString(3, obj.getEmail());
+			query.setString(4, obj.getDepartment());
+			query.setDouble(5, obj.getWage());
+			query.setInt(6, obj.getFranchise().getId());
+			query.setInt(7, obj.getId());
+
+			query.executeUpdate();
+			System.out.println("Update query succesfully executed.");
+
+		} catch (SQLException e) {
+			throw new DBException("Unexpected error: " + e.getMessage());
+		} finally {
+			DBManager.closeStatement(query);
+		}
+
 	}
 
 	@Override
 	public void deleteById(int id) {
+		PreparedStatement query = null;
+		try {
+			query = conn.prepareStatement("DELETE FROM employees WHERE ID = ?;");
+			query.setInt(1, id);
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DBManager.closeStatement(query);
+		}
+
 	}
 
 	@Override
@@ -38,7 +94,5 @@ public class EmployeeDAO implements IDAO<Employee> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	
 
 }
